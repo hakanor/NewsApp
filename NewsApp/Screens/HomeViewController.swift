38cd1c72830941267b5b733ad1db6bfac0612229
hakanor <hakanor99@gmail.com>
2022-07-21 00:46:24 +0300

    Added UITextField for HomePage

--- NewsApp/Screens/HomeViewController.swift
+++ NewsApp/Screens/HomeViewController.swift
@@ -9,20 +9,79 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-//    MARK: Subviews
+//    MARK: - Subviews
     private lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .plain)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .cyan
-        view.addSubview(tableView)
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = themeColors.blackPrimary
+        titleLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        titleLabel.text = "Browse"
+        return titleLabel
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let subtitleLabel = UILabel()
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textColor = themeColors.greyPrimary
+        subtitleLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        subtitleLabel.text = "Discover things of this world"
+        return subtitleLabel
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Search"
+        textField.backgroundColor = themeColors.greyPrimary
+        
+        textField.leftViewMode = UITextField.ViewMode.always
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let image = UIImage(systemName: "sun.max")
+        imageView.image = image
+        textField.leftView = imageView
+
+        textField.rightViewMode = UITextField.ViewMode.always
+        let imageViewRight = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let imageRight = UIImage(systemName: "sun.max")
+        imageViewRight.image = imageRight
+        textField.rightView = imageViewRight
+        
+        return textField
+       }()
+    
+//    MARK: - Configuration
+    func configurateTableView(){
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.frame = view.bounds
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = themeColors.white
+        [tableView, titleLabel, subtitleLabel, textField] .forEach(view.addSubview(_:))
+        configurateTableView()
+        
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20).isActive = true
+        
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        textField.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 32).isActive = true
+        textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        textField.heightAnchor.constraint(equalToConstant: 56).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: textField.bottomAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
     }
 
 }
