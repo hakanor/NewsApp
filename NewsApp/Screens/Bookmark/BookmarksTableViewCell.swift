7af586e3ca789dc6459873028689b5f12e504ff5
hakanor <hakanor99@gmail.com>
2022-07-27 15:43:13 +0300

    Custom cells for bookmark view controller

---
+++ NewsApp/Screens/Bookmark/BookmarksTableViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  BookmarksTableViewCell.swift
+//  NewsApp
+//
+//  Created by Hakan Or on 26.07.2022.
+//
+
+import Foundation
+import UIKit
+
+class BookmarksTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    private let cornerRadiusValue : CGFloat = 16
+    
+    // MARK: - Subviews
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = themeColors.white
+        return view
+    }()
+    
+    private lazy var articleImage: UIImageView = {
+        let articleImage = UIImageView()
+        articleImage.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(named: "bookmark")
+        articleImage.image = image
+        articleImage.contentMode = .scaleAspectFill
+        articleImage.backgroundColor = themeColors.white
+        articleImage.clipsToBounds = true
+        articleImage.layer.cornerRadius = cornerRadiusValue
+        return articleImage
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = themeColors.greyPrimary
+        titleLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        titleLabel.text = "UI/UX Design"
+        titleLabel.contentMode = .scaleToFill
+        titleLabel.numberOfLines = 1
+        return titleLabel
+    }()
+    
+    private lazy var subtitleLabel: UILabel = {
+        let subtitleLabel = UILabel()
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textColor = themeColors.blackPrimary
+        subtitleLabel.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        subtitleLabel.text = "A Simple Trick For creating Color Palettes Quickly"
+        subtitleLabel.contentMode = .scaleToFill
+        subtitleLabel.numberOfLines = 2
+        return subtitleLabel
+    }()
+    
+    // MARK: - Lifecycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        contentView.addSubview(containerView)
+        containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8).isActive = true
+        containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        
+        [titleLabel, subtitleLabel , articleImage] .forEach(containerView.addSubview(_:))
+
+        articleImage.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        articleImage.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
+        articleImage.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
+        articleImage.bottomAnchor.constraint(equalTo: containerView.bottomAnchor).isActive = true
+        
+        articleImage.heightAnchor.constraint(equalToConstant: 96).isActive = true
+        articleImage.widthAnchor.constraint(equalToConstant: 96).isActive = true
+        
+        titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor,constant: 8).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: articleImage.trailingAnchor,constant: 16).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -19).isActive = true
+
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        subtitleLabel.leadingAnchor.constraint(equalTo: articleImage.trailingAnchor,constant: 16).isActive = true
+        subtitleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor).isActive = true
+        subtitleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor,constant: -12).isActive = true
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    // MARK: - Configuration
+    func configureCells(with articleEntity: ArticleEntity){
+        titleLabel.text = articleEntity.title
+        subtitleLabel.text = articleEntity.subtitle
+        articleImage.imageFromUrl(from: articleEntity.urlToImage!,contentMode: .scaleAspectFill)
+    }
+    
+}
---
+++ NewsApp/Screens/Bookmark/BookmarksViewController.swift
@@ -0,0 +1,128 @@
+//
+//  BookmarksViewController.swift
+//  NewsApp
+//
+//  Created by Hakan Or on 16.07.2022.
+//
+
+import UIKit
+import CoreData
+
+class BookmarksViewController: UIViewController {
+
+    //    MARK: - ViewModels
+    private var items : [ArticleEntity]?
+        
+    //    MARK: - Subviews
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.layer.cornerRadius = 16
+        tableView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        return tableView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = themeColors.blackPrimary
+        titleLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        titleLabel.text = "Bookmarks"
+        return titleLabel
+    }()
+        
+    private lazy var subtitleLabel: UILabel = {
+        let subtitleLabel = UILabel()
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textColor = themeColors.greyPrimary
+        subtitleLabel.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        subtitleLabel.text = "Saved articles to the library"
+        return subtitleLabel
+    }()
+        
+        
+    //    MARK: - Configuration
+    func configurateTableView(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.showsVerticalScrollIndicator = false
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.separatorStyle = .none
+    }
+        
+    //    MARK: - Functions
+    private func fetchArticlesFromCoreData(){
+        let coreDataService = CoreDataService()
+        coreDataService.fetchArticlesFromCoreData()
+        items = coreDataService.items
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+        
+    //    MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchArticlesFromCoreData()
+        view.backgroundColor = themeColors.white
+        [tableView, titleLabel, subtitleLabel] .forEach(view.addSubview(_:))
+        configurateTableView()
+        
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20).isActive = true
+        
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8).isActive = true
+        subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 32).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor,constant: -20).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor,constant: 20).isActive = true
+        
+        tableView.register(BookmarksTableViewCell.self, forCellReuseIdentifier: "BookmarksTableViewCell")
+        
+    }
+
+}
+
+//    MARK: - TableView Delegate & TableView DataSource
+extension BookmarksViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookmarksTableViewCell") as! BookmarksTableViewCell
+        cell.configureCells(with: (items?[indexPath.row])!)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            guard let itemsToBeDeleted = items?[indexPath.row] else { return  }
+            items?.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            let coreDataService = CoreDataService()
+            coreDataService.deleteFromCoreData(with: itemsToBeDeleted)
+            DispatchQueue.main.async {
+                tableView.reloadData()
+            }
+
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = items![indexPath.row]
+        let vc = ArticleViewController(
+            title: item.title ?? "",
+            source: item.source ?? "",
+            content: item.content ?? "",
+            imageUrl: item.urlToImage ?? "",
+            url: item.url ?? ""
+        )
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+}
